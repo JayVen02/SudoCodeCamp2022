5e4386edb418c87a381f2de1c0e859917d652434
Joven Rosales <104308582+JayVen02@users.noreply.github.com>
2022-10-03 14:49:23 +0800

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,15 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		int[] numbers = {3, 9, 4, 13};
+	   
+	    for (int number: numbers) {
+	      System.out.println(number);
+	    }
+	  }
+	
+
+	}
+
+
